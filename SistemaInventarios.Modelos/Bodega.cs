c224fbc34cc9bb893fc89773fdd7936b3c823ea0
davidgomez <algt0721@gmail.com>
2023-10-05 15:55:40 -0600

    Configuracion del Proyecto

---
+++ SistemaInventarios.Modelos/Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarios.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60,ErrorMessage ="Nombre debe ser Maximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerido")]
+        [MaxLength(100, ErrorMessage = "Nombre debe ser Maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es Requerido")]
+        public bool Estado { get; set;}
+
+    }
+}
